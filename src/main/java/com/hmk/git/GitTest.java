ef38a3ab094d19c6428a0214af3b86947b486510
Hmmk <635221624@.com>
2022-03-22 21:33:37 +0800

    first commit

---
+++ src/main/java/com/hmk/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hmk.git;
+
+/**
+ * @author 侯孟珂
+ * @date 2022/3/22-21:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
